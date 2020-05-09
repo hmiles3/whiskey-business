8a763c1f38101bec82dc90204ca16e8737f31015
Hank Miles <Hankshadow2000@yahoo.com>
2020-05-09 14:02:30 -0400

    html changes

---
+++ style.js
@@ -0,0 +1,12 @@
+// <!-- ajax call "thecocktaildb.com/api/json/v1/1/filter.php?i="+ingredient -->
+    // <!-- 
+    //     call drinks[i].strDrink
+    //  -->
+// <!-- ajax call "thecocktaildb.com/api/json/v1/1/search/php?s="+cocktail -->
+//     <!-- 
+//         call strDrink and saveButton
+//         call strDrinkThumb
+//         call strGlass
+//         call strIngredient[1-15] and strMeasure[1-15]
+//         call strInstructions
+//     -->
