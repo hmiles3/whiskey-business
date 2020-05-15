3cf6d98c4dd9c2b43c05e4518229a6a88770be00
Hank Miles <Hankshadow2000@yahoo.com>
2020-05-15 09:29:57 -0400

    fixed a bug involving ingredients with no listed measurements

--- style.js
+++ style.js
@@ -69,49 +69,49 @@ function findRecipe(cocktail){
         src.appendChild(img)
         $("#cocktailGlass").text("Glass type: "+response.drinks[0].strGlass);
         $("#cocktailIngredient").text("Ingredients:")
-        if (response.drinks[0].strIngredient1 && response.drinks[0].strMeasure1 !== null){
+        if (response.drinks[0].strIngredient1 || response.drinks[0].strMeasure1 !== null){
             $("#cocktailIngredient1").text(response.drinks[0].strIngredient1+": "+response.drinks[0].strMeasure1);
             }
-        if (response.drinks[0].strIngredient2 && response.drinks[0].strMeasure2 !== null){
+        if (response.drinks[0].strIngredient2 || response.drinks[0].strMeasure2 !== null){
             $("#cocktailIngredient2").text(response.drinks[0].strIngredient2 + ": "+response.drinks[0].strMeasure2);
             }
-        if (response.drinks[0].strIngredient3 && response.drinks[0].strMeasure3 !== null){
+        if (response.drinks[0].strIngredient3 || response.drinks[0].strMeasure3 !== null){
             $("#cocktailIngredient3").text(response.drinks[0].strIngredient3 + ": "+response.drinks[0].strMeasure3);
             }
-        if (response.drinks[0].strIngredient4 && response.drinks[0].strMeasure4 !== null){
+        if (response.drinks[0].strIngredient4 || response.drinks[0].strMeasure4 !== null){
             $("#cocktailIngredient4").text(response.drinks[0].strIngredient4 + ": "+response.drinks[0].strMeasure4);
             }
-        if (response.drinks[0].strIngredient5 && response.drinks[0].strMeasure5 !== null){
+        if (response.drinks[0].strIngredient5 || response.drinks[0].strMeasure5 !== null){
             $("#cocktailIngredient5").text(response.drinks[0].strIngredient5 + ": "+response.drinks[0].strMeasure5);
             }
-        if (response.drinks[0].strIngredient6 && response.drinks[0].strMeasure6 !== null){
+        if (response.drinks[0].strIngredient6 || response.drinks[0].strMeasure6 !== null){
             $("#cocktailIngredient6").text(response.drinks[0].strIngredient6 + ": "+response.drinks[0].strMeasure6);
             }
-        if (response.drinks[0].strIngredient7 && response.drinks[0].strMeasure7 !== null){
+        if (response.drinks[0].strIngredient7 || response.drinks[0].strMeasure7 !== null){
             $("#cocktailIngredient7").text(response.drinks[0].strIngredient7 + ": "+response.drinks[0].strMeasure7);
             }
-        if (response.drinks[0].strIngredient8 && response.drinks[0].strMeasure8 !== null){
+        if (response.drinks[0].strIngredient8 || response.drinks[0].strMeasure8 !== null){
             $("#cocktailIngredient8").text(response.drinks[0].strIngredient8 + ": "+response.drinks[0].strMeasure8);
             }
-        if (response.drinks[0].strIngredient9 && response.drinks[0].strMeasure9 !== null){
+        if (response.drinks[0].strIngredient9 || response.drinks[0].strMeasure9 !== null){
             $("#cocktailIngredient9").text(response.drinks[0].strIngredient9 + ": "+response.drinks[0].strMeasure9);
             }
-        if (response.drinks[0].strIngredient10 && response.drinks[0].strMeasure10 !== null){
+        if (response.drinks[0].strIngredient10 || response.drinks[0].strMeasure10 !== null){
             $("#cocktailIngredient10").text(response.drinks[0].strIngredient10 + ": "+response.drinks[0].strMeasure10);
             }
-        if (response.drinks[0].strIngredient11 && response.drinks[0].strMeasure11 !== null){
+        if (response.drinks[0].strIngredient11 || response.drinks[0].strMeasure11 !== null){
             $("#cocktailIngredient11").text(response.drinks[0].strIngredient11 + ": "+response.drinks[0].strMeasure11);
             }
-        if (response.drinks[0].strIngredient12 && response.drinks[0].strMeasure12 !== null){
+        if (response.drinks[0].strIngredient12 || response.drinks[0].strMeasure12 !== null){
             $("#cocktailIngredient12").text(response.drinks[0].strIngredient12 + ": "+response.drinks[0].strMeasure12);
             }
-        if (response.drinks[0].strIngredient13 && response.drinks[0].strMeasure13 !== null){
+        if (response.drinks[0].strIngredient13 || response.drinks[0].strMeasure13 !== null){
             $("#cocktailIngredient13").text(response.drinks[0].strIngredient13 + ": "+response.drinks[0].strMeasure13);
             }
-        if (response.drinks[0].strIngredient14 && response.drinks[0].strMeasure14 !== null){
+        if (response.drinks[0].strIngredient14 || response.drinks[0].strMeasure14 !== null){
             $("#cocktailIngredient14").text(response.drinks[0].strIngredient14 + ": "+response.drinks[0].strMeasure14);
             }
-        if (response.drinks[0].strIngredient15 && response.drinks[0].strMeasure15 !== null){
+        if (response.drinks[0].strIngredient15 || response.drinks[0].strMeasure15 !== null){
             $("#cocktailIngredient15").text(response.drinks[0].strIngredient15 + ": "+response.drinks[0].strMeasure15);
             }
         $("#cocktailInstructions").text("Instructions: "+response.drinks[0].strInstructions)
